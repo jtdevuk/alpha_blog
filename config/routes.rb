66b92b4f9eb901fbb5d02fe60661029e36858b91
John Tweeddale <foureggs@hotmail.com>
2020-04-14 14:13:21 +0100

    Add user sign up feature

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to:'pages#about'
   resources :articles
+  resources :users, except: [:new]
+  get 'signup', to: 'users#new'
   
 end
