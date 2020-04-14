42351ddd6f9456cd7cd0e3faed89111795ecd127
John Tweeddale <foureggs@hotmail.com>
2020-04-14 10:41:13 +0100

    Add BCrypt functionality

--- app/models/concerns/user.rb
+++ app/models/concerns/user.rb
@@ -6,4 +6,5 @@ before_save { self.email = email.downcase}
 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 validates :email, presence: true,
     uniqueness: { case_sensitive: false }, length: { maximum: 105 }, format: {with: VALID_EMAIL_REGEX}
+  has_secure_password
 end
