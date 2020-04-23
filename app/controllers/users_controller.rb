b975711e617b157eca7fdfc4afc98daf1ee9400f
John Tweeddale <foureggs@hotmail.com>
2020-04-23 19:11:42 +0100

    Add admin ability to delete posts and users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
   before_action :require_same_user, only: [:edit, :update]
+  before_action :require_admin, only: [:destroy]
 
   def new
     @user = User.new
@@ -37,22 +38,39 @@ class UsersController < ApplicationController
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    if @user != current_user
+      @user.destroy
+      flash[:danger] = "User and all articles created by user have been deleted"
+      redirect_to users_path
+    else
+      flash[:danger] = "You can't delete yourself!"
+      redirect_to users_path
+    end
+  end
+
   private
 
     def user_params
     params.require(:user).permit(:username, :email, :password)
     end
-end
-
-private
-
-def set_user
-  @user = User.find(params[:id])
-end
-
-def require_same_user
-  if logged_in? && current_user != @user
-    flash[:danger] = "You can only edit your own account"
-    redirect_to root_path
   end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user and !current_user.admin?
+      flash[:danger] = "You can only edit your own account"
+      redirect_to root_path
+    end
+  end
+
+  def require_admin
+    if logged_in? and !current_user.admin?
+      flash[:danger] = "Only admin users can perform that action"
+      redirect_to root_path
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_14_093057) do
+ActiveRecord::Schema.define(version: 2020_04_23_112632) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_04_14_093057) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
