0c142d3096538b1d44fd592e5fe00752e1c10874
John Tweeddale <foureggs@hotmail.com>
2020-05-14 15:21:34 +0100

    Add categories routes and views, add tests to categories controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to:'pages#about'
   resources :articles
+  resources :categories, except: [:destroy]
   resources :users, except: [:new]
   get 'signup', to: 'users#new'
   get 'login', to: 'sessions#new'
